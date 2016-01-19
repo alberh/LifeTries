46fcf5ec930450811bb2e86771f7e59fe9f40a45
Alberto García <alberto.gglez@gmail.com>
2016-01-19 21:05:41 +0100

    Todo el proyecto.

---
+++ android/src/com/lifetries/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lifetries;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lifetries.LifeTries;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LifeTries(), config);
+	}
+}
---
+++ html/src/com/lifetries/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.lifetries.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.lifetries.LifeTries;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LifeTries();
+        }
+}
